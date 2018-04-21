1f7360b9b56d73a62435c4841c529edb5a7d04ee
Neda Barqawi <nedabarqawi@Nedas-MacBook-Pro.local>
2018-04-21 13:25:40 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
 
+// Create an array for all cards
+var cards = ['queen', 'queen', 'king', 'king'];
+
+// creat an array to hold the cards that are in play.
+var cardsInPlay = [];
+
+// create a variable to represent the first card.
+var cardOne = cards[0];
+
+// add card 1 to the cardsInPlay array.
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+
+
+// create a variable to represent the seconed card.
+var cardTwo = cards[2];
+
+// add card 2 to the cardsInPlay array
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+
+// if two cards match. 
+if (cardsInPlay.length === 2) {
+
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
