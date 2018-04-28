e25e573923e9457179448f89aac1b67e87b8e840
Neda Barqawi <nedabarqawi@Nedas-MacBook-Pro.local>
2018-04-28 14:02:15 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// create four objects, one for each card.
+// array to stor all cards---four objects, one for each card.
 var cards = [
   {
     rank: 'queen',
@@ -23,50 +23,77 @@ var cards = [
   }
 ];
 
-// creat an array to hold the cards that are in play.
+// array to hold the cards that are in play.
 var cardsInPlay = [];
 
 
-// function to check the matching
-var checkForMatch = function() {
+// function to store the steps to check for a match.
+var checkForMatch = function () {
 
-// if two cards match 
+
+// Check to see if two cards match
 if (cardsInPlay[0] === cardsInPlay[1]) {
- alert ("You found a match!");
-} else {
-  alert ("Sorry, try again.");
-
-}
-
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 };
 
 
 
-// function for flipping a card.
+// function to store all steps that should happen when the user flips a card.
 
-var flipCard = function (cardId) {
+var flipCard = function () {
 
-console.log ("User flipped " + cards[cardId].rank);
+// Get the data-id of the card.
+cardId = this.getAttribute('data-id');
+  console.log(cardId);
 
-cardsInPlay.push (cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 
-console.log(cards[cardId].cardImage)
-console.log(cards[cardId].suit)
+// Display the card's image
+  this.setAttribute('src', cards[cardId].cardImage);
 
 
-// if two cards played. 
+// Check if user played two cards. 
 if (cardsInPlay.length === 2) {
 
-// call the checkForMatch function.
+// call checkForMatch function, after user played two cards
  checkForMatch();
+
 
 }
 
 };
 
-// call for flipCard function,arguments is represent the index numbers of card array.
-flipCard(0);
-flipCard(2);
 
+// function to create a new game board
+var createBoard = function () {
+
+// loop through the cards array.
+for (var i = 0; i < cards.length; i++) {
+  // create an img element 
+  var cardElement = document.createElement('img');
+
+// adding src attribute & attribute value 
+  cardElement.setAttribute('src', 'images/back.png');
+
+// Set the card's 'data-id' attribute to be the index of the current element.
+  cardElement.setAttribute('data-id', i);
+
+// add a click event to the cardElement.
+cardElement.addEventListener('click', flipCard);
+
+
+// append the current cardElement to the game board
+   document.getElementById('game-board').appendChild(cardElement);
+
+}
+
+
+}
+
+// call the createBoard function to create the board
+createBoard();
 
 
